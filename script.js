0a055ad440c8b1487ad9ff3016729486cdfd768c
eduarda rodrigues <eduardarodrigue0303@gmail.com>
2025-06-09 00:13:40 +0000

    correção de erros e mudanças em algumas coisas do codigo

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const botaoAbrir = document.querySelector("header > button")
-const botaoFechar = document.querySelector ("header  nav button")
+const botaoFechar = document.querySelector("header nav button")
 
-const body = document.querySelector("body")
-const nav = document.querySelector("header .mobile ")
+const body= document.querySelector("body")
+
+const nav = document.querySelector("header .mobile")
 
 botaoAbrir.addEventListener('click', abrirMenu)
-botaoAbrir.addEventListener('click', fecharMenu)
+botaoFechar.addEventListener('click', fecharMenu)
 
-
-function abrirMenu() {
-  body.classList("escurecer")
-  nav.classList.add("abrir")
+function abrirMenu(){
+   body.classList.add("escurecer")
+   nav.classList.add("abrir")
 }
 
-function fecharMenu() {
-  body.classList.remove("escurecer")
-  nav.classList.remove("abrir")
+function fecharMenu(){
+   body.classList.remove("escurecer")
+   nav.classList.remove("abrir")
 }
